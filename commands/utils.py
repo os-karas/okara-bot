72fda5256e8db9fbf48ca634aba676aa5d42eab3
kauaug <kauaug.mo@gmail.com>
2022-11-15 15:00:41 -0300

    feat(commands-utils): calculator

--- commands/utils.py
+++ commands/utils.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands
 from discord.embeds import Embed
 
+from errors.events import IntentionalError
+
 
 class Utils(commands.Cog):
     """utilities Commands"""
@@ -18,6 +20,28 @@ class Utils(commands.Cog):
             description=f"{self.bot.latency*1000:.0f}ms",
             color=discord.Color.dark_grey(),
         ))
+
+    @commands.command(name="calc", aliases=["calculate", "calculator"])
+    async def calculate(self, ctx: commands.Context, *, calc: str):
+        """calculate math expressions"""
+
+        if calc.__len__() > 50:
+            raise IntentionalError("The accepted character limit is 50.")
+
+        await ctx.send(embed=Embed(
+            title=calc,
+            description=eval(calc),
+            color=discord.Color.dark_green()
+        ))
+
+    @calculate.error
+    async def calculate_error(self, ctx: commands.Context, err: commands.CommandError):
+        if isinstance(err, commands.CommandInvokeError):
+            ctx.command_failed = False
+            return await ctx.message.reply("Invalid calculation, enter a valid calculation.")
+        if isinstance(err, commands.MissingRequiredArgument):
+            ctx.command_failed = False
+            return await ctx.message.reply("the calculation is a required argument.",)
 
 
 async def setup(bot: commands.Bot):
